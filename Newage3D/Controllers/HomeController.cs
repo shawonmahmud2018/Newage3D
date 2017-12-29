8c1c90f6ba5c1b151830d3534b53cb275ed599a2
shawonmahmud2018 <shawon.csesust@gmail.com>
2017-12-29 16:27:27 +0600

    Changes on home controller area

--- Newage3D/Controllers/HomeController.cs
+++ Newage3D/Controllers/HomeController.cs
@@ -13,20 +13,6 @@ namespace Newage3D.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult Images()
         {
             ViewBag.Message = "Your images page.";
